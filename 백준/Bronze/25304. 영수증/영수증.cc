cbae3d1823e74dd0adf3315e876fdfe07416bf6f
이남진 <s25028@gsm.hs.kr>
2025-09-28 00:00:28 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int X, N, a, b;
+    scanf("%d", &X);
+    scanf("%d", &N);
+    
+    for(int i = 0; i < N; i++) {
+        scanf("%d %d", &a, &b);
+        X -= a * b;
+    }
+    if(X == 0) {
+        printf("Yes");
+    }
+    else {
+        printf("No");
+    }
+}
