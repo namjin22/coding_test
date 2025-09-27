8ec30b27f397d5e162f17191d48336adced2091c
이남진 <s25028@gsm.hs.kr>
2025-09-27 23:25:02 +0900

    adsf

--- python_coding_test.py
+++ python_coding_test.py
@@ -1,10 +1,46 @@
-test = int(input())
+n = int(input())
 
-for a in range(test):
-    R, S = input().split()
-    R = int(R)
-    
-    for b in S:
-        print(b * R, end = "")
+if n % 5 == 0:  # 5으로 나눠떨어질 때
+    print(n // 5)
+else:
+    p = 0
+    while n > 0:
+        n -= 3
+        p += 1
         
-    print()
+        if n % 5 == 0:  # 3kg과 5kg를 조합해서 담을 수 있을 때
+            p += n // 5
+            print(p)
+            break
+
+        elif n == 1 or n == 2:  # 설탕 봉지만으로 나눌 수 없을 때
+            print(-1)
+            break
+
+        elif n == 0:  # 3으로 나눠떨어질 때
+            print(p)
+            break
+
+n = int(input())
+
+if n % 5 == 0:
+    print(n // 5)
+    
+else:
+    p = 0
+    while n > 0:
+        n -= 3
+        p += 1
+        
+        if n % 5 == 0:
+            p += n // 5
+            print(p)
+            break
+            
+        elif n == 1 or n == 2:
+            print(-1)
+            break
+            
+        elif n == 0:
+            print(p)
+            break
